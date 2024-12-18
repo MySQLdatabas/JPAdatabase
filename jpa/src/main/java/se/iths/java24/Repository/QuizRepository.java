9d0e7acebc39e2a6c79ad695cc570f78bd5b6310
kirre02 <74262686+kirre02@users.noreply.github.com>
2024-12-18 19:07:09 +0100

    inital work on Quiz repository

--- jpa/src/main/java/se/iths/java24/Repository/QuizRepository.java
+++ jpa/src/main/java/se/iths/java24/Repository/QuizRepository.java
@@ -1,4 +1,30 @@
 package se.iths.java24.Repository;
 
+import se.iths.java24.Entity.Quiz;
+import se.iths.java24.JPAUtil;
+
+import java.util.List;
+
 public class QuizRepository {
+    public void createQuiz(Quiz quiz) {
+        JPAUtil.inTransaction(em -> em.persist(quiz));
+    }
+
+    public Quiz getQuizById(int id) {
+        return JPAUtil.getEntityManager().find(Quiz.class, id);
+    }
+
+    public List<Quiz> getAllQuiz() {
+        return JPAUtil.getEntityManager()
+                .createQuery("select title, description from Quiz", Quiz.class)
+                .getResultList();
+    }
+
+    public void updateQuiz(Quiz quiz) {
+        JPAUtil.inTransaction(em -> em.merge(quiz));
+    }
+
+    public void deleteQuiz(int id) {
+        JPAUtil.inTransaction(em -> em.remove(em.find(Quiz.class, id)));
+    }
 }
