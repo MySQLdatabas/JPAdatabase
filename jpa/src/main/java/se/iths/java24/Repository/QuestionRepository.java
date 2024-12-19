917d303376a9b525a521a3ec158f2deeef28b20c
PatriciaNilsson <patricia.nilsson@iths.se>
2024-12-19 12:45:31 +0100

    Question Repository

--- jpa/src/main/java/se/iths/java24/Repository/QuestionRepository.java
+++ jpa/src/main/java/se/iths/java24/Repository/QuestionRepository.java
@@ -3,8 +3,12 @@ package se.iths.java24.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import se.iths.java24.Entity.Question;
+import se.iths.java24.JPAUtil;
+
 import java.util.List;
 
-public interface QuestionRepository extends JpaRepository<Question, Long> {
-    List<Question> findByQuizQuiz_id(Long quizId);
+public class QuestionRepository  {
+    public List<Question> listQuestions(int quizId) {
+        return JPAUtil.getEntityManager().createQuery("SELECT q.text, q.difficulty FROM Question q JOIN Quiz qu on q.quiz_id = :quizId ORDER BY difficulty", Question.class).getResultList();
+    }
 }
