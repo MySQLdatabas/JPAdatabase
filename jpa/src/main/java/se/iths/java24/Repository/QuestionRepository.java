6e492d98234ddba940fbbe4ce1354f0be195201e
PatriciaNilsson <patricia.nilsson@iths.se>
2024-12-19 13:05:03 +0100

    Question Repository

--- jpa/src/main/java/se/iths/java24/Repository/QuestionRepository.java
+++ jpa/src/main/java/se/iths/java24/Repository/QuestionRepository.java
@@ -8,6 +8,19 @@ import se.iths.java24.JPAUtil;
 import java.util.List;
 
 public class QuestionRepository {
+
+    public void createQuestion(Question question) {
+        JPAUtil.inTransaction(em -> em.persist(question));
+    }
+
+    public void deleteQuestion(Question question) {
+        JPAUtil.inTransaction(em -> em.remove(question));
+    }
+
+    public void updateQuestion(Question question) {
+        JPAUtil.inTransaction(em -> em.merge(question));
+    }
+
     public List<Question> listQuestions(int quizId) {
         return JPAUtil.getEntityManager()
                 .createQuery("SELECT q.text, q.difficulty FROM Question q JOIN Quiz qu on q.quiz_id = qu.quiz_id WHERE qu.quiz_id = :quizId ORDER BY difficulty", Question.class)
