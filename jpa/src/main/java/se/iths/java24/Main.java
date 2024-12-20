a221bbdd35b1b26d13beb85f8a5382f858e71e31
Tova <tova.bryngelsson@live.se>
2024-12-20 09:57:54 +0100

    CRUD functions and answer menu

--- jpa/src/main/java/se/iths/java24/Main.java
+++ jpa/src/main/java/se/iths/java24/Main.java
@@ -1,8 +1,12 @@
 package se.iths.java24;
 
 import jakarta.persistence.EntityManager;
+import se.iths.java24.Entity.Answer;
+import se.iths.java24.Entity.Question;
 import se.iths.java24.Entity.User;
+import se.iths.java24.Repository.AnswerRepository;
 
+import java.util.List;
 import java.util.Scanner;
 
 
@@ -12,6 +16,8 @@ public class Main {
 
     public static void main(String[] args) {
         Boolean quit = false;
+
+        printAction();
 
         while (!quit) {
             int action = sc.nextInt();
@@ -118,6 +124,7 @@ public class Main {
     }
 
     private static void manageAnswers(Scanner sc) {
+        AnswerRepository answerRepository = new AnswerRepository();
         System.out.println("\nManage answers:");
         System.out.println(""" 
                     1 - View answer
@@ -134,10 +141,72 @@ public class Main {
                 switch (action) {
                     case 0 -> {System.out.println("\nReturning to main menu...");
                         return;}
-                    case 1 -> {}
-                    case 2 -> {}
-                    case 3 -> {}
-                    case 4 -> {}
+                    case 1 -> {
+                        System.out.println("\nEnter id for the question you want answers for");
+                        int questionId = sc.nextInt();
+                        List<Answer> answers = answerRepository.listAnswers(questionId);
+                        answers.forEach(answer ->
+                                System.out.println("ID: " + answer.getId() +
+                                        ", Answer:" + answer.getAnswerText() +
+                                        ", Correct: " + answer.getIsCorrect())
+                                );
+                    }
+                    case 2 -> {
+                        System.out.println("\nEnter the answer:");
+                        String answerText = sc.nextLine();
+                        System.out.println("\nIs the answer true or false?");
+                        Boolean isCorrectAnswer = Boolean.valueOf(sc.nextLine());
+                        System.out.println("\nEnter Question ID for your answer:");
+                        int questionId = sc.nextInt();
+                        Question question = answerRepository.getQuestionById(questionId);
+
+                        if (question != null) {
+                            Answer newAnswer = new Answer();
+                            newAnswer.setIsCorrect(isCorrectAnswer);
+                            newAnswer.setAnswerText(answerText);
+                            newAnswer.setQuestion(question);
+
+                            answerRepository.createAnswer(newAnswer);
+                            System.out.println("\nNew answer created successfully");
+                        } else {
+                            System.out.println("Question not found with ID: " + questionId);
+                        }
+                    }
+                    case 3 -> {
+                        System.out.println("\nEnter answer id:");
+                        int answerId = sc.nextInt();
+                        sc.nextLine();
+                        Answer answerToUpdate = AnswerRepository.getAnswerById(answerId);
+
+                        if (answerToUpdate == null) {
+                            System.out.println("Answer not found.");
+                            break;
+                        }
+
+                        System.out.println("Enter new answer (leave blank to keep current):");
+                        String newAnswerText = sc.nextLine();
+
+                        if (!newAnswerText.isBlank()) {
+                            answerToUpdate.setAnswerText(newAnswerText);
+                        }
+
+                        System.out.println("Enter if answer is true or false:");
+                        Boolean newIsCorrect = Boolean.valueOf(sc.nextLine());
+                        answerToUpdate.setIsCorrect(newIsCorrect);
+
+                        answerRepository.updateAnswer(answerToUpdate);
+                        System.out.println("\nNew answer created successfully");
+
+                        //QuestionId is the same as before
+
+                    }
+                    case 4 -> {
+                        System.out.println("\nEnter answer id to delete:");
+                        int answerId = sc.nextInt();
+                        sc.nextLine();
+                        answerRepository.deleteAnswer(answerId);
+                        System.out.println("\nAnswer deleted successfully");
+                    }
                     default -> System.out.println("\nOgiltigt val, försök igen");
                 }
             }
